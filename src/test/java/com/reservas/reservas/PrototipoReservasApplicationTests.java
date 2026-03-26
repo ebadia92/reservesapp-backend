bd2f3410031502cc1e421485d79c5e8b6ce5d32c
Daniela Gutierrez Aliaga <daniela_gutierrez_aliaga@hotmail.com>
2026-03-26 22:31:34 +0100

    feat: creación de la estructura base del backend de RoomyApp

---
+++ src/test/java/com/reservas/reservas/PrototipoReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototipoReservasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

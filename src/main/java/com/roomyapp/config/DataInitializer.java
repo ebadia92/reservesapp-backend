6a719a660e46641977cd7656409bb628e8728b25
Esteve Badia <estevebadia1992@gmail.com>
2026-03-30 20:22:16 +0200

    Actualizado el DataInitializer para que no cree un admin por defecto, ya que está creado en la base de datos.

--- src/main/java/com/roomyapp/config/DataInitializer.java
+++ src/main/java/com/roomyapp/config/DataInitializer.java
@@ -1,11 +1,11 @@
 package com.roomyapp.config;
 
-import com.roomyapp.entity.User;
-import com.roomyapp.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.roomyapp.repository.UserRepository;
 
 /*
 Clase que crea un usuario Admin si no existe.
@@ -18,21 +18,6 @@ public class DataInitializer {
     CommandLineRunner initAdmin(UserRepository userRepository,
                                 PasswordEncoder passwordEncoder){
         return args -> {
-
-            if (userRepository.findByEmail("admin@test.com").isEmpty()) {
-
-                //Creamos usuario Admin
-                User admin = new User();
-                admin.setName("Admin");
-                admin.setEmail("admin@test.com");
-                admin.setPassword(passwordEncoder.encode("1234"));
-                admin.setRole(User.Role.ADMIN);
-
-                userRepository.save(admin);
-                System.out.println("Admin creado automáticamente");
-            } else{
-                System.out.println("Admin ya existe");
-            }
         };
     }
 }
